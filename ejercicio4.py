ee7c6ca8b66111c45c5b492c3511045da26649f7
almdzahir <zahiralmada@uca.edu.ar>
2025-04-03 18:41:50 -0300

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+clasificacion = 5
+
+if clasificacion >= 90:
+    print("Excelente")
+elif clasificacion >= 80:
+    print("Muy Bueno")
+elif clasificacion >= 70:
+    print("Bueno")
+elif clasificacion < 70:
+    print("TUNG TUNG TUNG TUNG TUNG TUNG SAHUR")
---
+++ ejercicio6.py
@@ -0,0 +1,35 @@
+'''
+Este modulo
+calcula la edad de un alumno
+cuando ingresa el año de nacimiento
+'''
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+int = integer
+edad = 18 #indica la edad
+
+float = flotante o real:
+precio = 9.99
+altura = 1.75
+
+cadeba de texto = string 
+nombre = "Zahir"
+mensaje = "Hola" 'Mundo'
+
+Booleanos = Bool 
+
+mayor_ edad=True; mayor_edad= False 
+
+if edad = 18
+if edad == 18
+
+a = b = c = 10
+
+edad= input("Ingrese su edad: ")
