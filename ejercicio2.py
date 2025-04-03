9a343339d4a4a359612d23a8234a06b31c80fedc
almdzahir <zahiralmada@uca.edu.ar>
2025-04-03 18:36:05 -0300

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,8 @@
+a = input("Ingresa el valor de a: ")
+b = input("Ingresa el valor de b: ")
+
+print(f"Antes del intercambio: a = {a}, b = {b}")
+
+a, b =b, a 
+
+print(f"Despues del intercambio: a = {a}, b = {b}")
