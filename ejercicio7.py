30d9e3d96be6a6a538d36039efee8a5e43732e45
almdzahir <zahiralmada@uca.edu.ar>
2025-04-03 19:37:48 -0300

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,26 @@
+frutas = ["manzana", "pera", "uva", "mandarina"]
+frutas.append("banana")
+#print(fruta[1])
+frutas.insert(2, "Durazno")
+frutas.remove("Uva")
+frutas.sort()
+frutas.reverse()
+for fruta in frutas:
+    print(fruta)
+ 
+ 
+ 
+'''
+
+#while #repite un bloque mientras una condicion sea verdadera
+# contador = 3 // asigno un valor a la variable
+# contador == 5 // hacer una comparacion
+
+
+contador=0
+while True:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break 
+'''
